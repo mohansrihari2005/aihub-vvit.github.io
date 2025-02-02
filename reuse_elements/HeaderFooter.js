2dca97cbfc22cf00cb0cca46b71abd4849bbc82f
Kasa-Harendra <harendrakasa@gmail.com>
2025-02-03 01:11:04 +0530

    extended

--- reuse_elements/HeaderFooter.js
+++ reuse_elements/HeaderFooter.js
@@ -4,8 +4,8 @@ class SpecialHeader extends HTMLElement{
         <!-- Navigation bar -->
         <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
             <div class="container-fluid">
-                <div>
-                    <!-- <img src="./reuse_elements/AI-HUB.png" alt="AI-HUB" width="30px"> -->
+                <div style="display: flex; align-items: center">
+                    <img src="./images/AI-HUB.jpg" alt="AI-HUB" width="30px" style="border-radius: 50%;">
                     <a class="navbar-brand" href="#">AI-HUB</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,7 +29,8 @@ class Links extends HTMLElement{
         this.innerHTML=`
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=domain" />`
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=domain" />
+        <link rel="shortcut icon" href="./images/AI-HUB.jpg" type="image/jpg">`
     }
 }
 
