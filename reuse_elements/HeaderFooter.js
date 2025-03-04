b96ee1604be5ff8c1b81ee5b91ccd33806ed16b4
Vamsi Krishna Reddy <92618970+69492@users.noreply.github.com>
2025-03-04 20:40:31 +0530

    Update HeaderFooter.js

--- reuse_elements/HeaderFooter.js
+++ reuse_elements/HeaderFooter.js
@@ -5,7 +5,7 @@ class SpecialHeader extends HTMLElement {
         <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
             <div class="container-fluid">
                 <div style="display: flex; align-items: center">
-                    <img src="./images/AI-HUB.jpg" alt="AI-HUB@VVIT" width="35px" style="border-radius: 60%;">
+                    <img src="images/AI-HUB.jpg" alt="AI-HUB@VVIT" width="35px" style="border-radius: 60%;">
                     <a class="navbar-brand" href="#">AI-HUB@VVIT</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,12 +21,18 @@ class SpecialHeader extends HTMLElement {
                                 Projects
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="projectsDropdown">
-                                <li><a class="dropdown-item" href="./Projects.html">Ongoing Projects</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><h6 class="dropdown-header">Completed Projects</h6></li>
-                                <li><a class="dropdown-item ps-4" href="./Game.html">Games</a></li>
-                                <li><a class="dropdown-item ps-4" href="Projects.html#ml">Machine Learning</a></li>
-                                <li><a class="dropdown-item ps-4" href="Projects.html#dl">Deep Learning</a></li>
+                                <li><a class="dropdown-item" href="Projects.html">Ongoing Projects</a></li>
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#" id="completedProjectsDropdown" aria-expanded="false">
+                                        Completed Projects
+                                        <i class="fas fa-chevron-right submenu-arrow"></i>
+                                    </a>
+                                    <ul class="dropdown-menu submenu" aria-labelledby="completedProjectsDropdown">
+                                        <li><a class="dropdown-item" href="Game.html">Games</a></li>
+                                        <li><a class="dropdown-item" href="Projects.html#ml">Machine Learning</a></li>
+                                        <li><a class="dropdown-item" href="Projects.html#dl">Deep Learning</a></li>
+                                    </ul>
+                                </li>
                             </ul>
                         </li>
                         
@@ -44,14 +50,19 @@ class SpecialHeader extends HTMLElement {
                                 Career Catalysts
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="careerDropdown">
-                                <li><a class="dropdown-item" href="./Career.html">Job Guide</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><h6 class="dropdown-header">Life Skills</h6></li>
-                                <li><a class="dropdown-item ps-4" href="#">Quantitative Aptitude</a></li>
-                                <li><a class="dropdown-item ps-4" href="#">Reasoning Ability</a></li>
-                                <li><a class="dropdown-item ps-4" href="#">Verbal Ability</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="./Career.html">Web Dev</a></li>
+                                <li><a class="dropdown-item" href="Career.html">Job Guide</a></li>
+                                <li class="dropdown-submenu">
+                                    <a class="dropdown-item dropdown-toggle" href="#" id="lifeSkillsDropdown" aria-expanded="false">
+                                        Life Skills
+                                        <i class="fas fa-chevron-right submenu-arrow"></i>
+                                    </a>
+                                    <ul class="dropdown-menu submenu" aria-labelledby="lifeSkillsDropdown">
+                                        <li><a class="dropdown-item" href="#">Quantitative Aptitude</a></li>
+                                        <li><a class="dropdown-item" href="#">Reasoning Ability</a></li>
+                                        <li><a class="dropdown-item" href="#">Verbal Ability</a></li>
+                                    </ul>
+                                </li>
+                                <li><a class="dropdown-item" href="Career.html">Web Dev</a></li>
                             </ul>
                         </li>
                         
@@ -61,22 +72,59 @@ class SpecialHeader extends HTMLElement {
                     </ul>
                 </div>
             </div>
-        </nav>`
+        </nav>
+        <style>
+            .dropdown-submenu {
+                position: relative;
+            }
+            
+            .dropdown-submenu .submenu {
+                top: 0;
+                left: 100%;
+                margin-top: -1px;
+                display: none;
+                position: absolute;
+            }
+            
+            .submenu-arrow {
+                float: right;
+                margin-top: 4px;
+            }
+            
+            @media (min-width: 992px) {
+                .dropdown-submenu:hover .submenu {
+                    display: block;
+                }
+            }
+            
+            @media (max-width: 991px) {
+                .dropdown-submenu .submenu {
+                    position: static;
+                    margin-left: 15px;
+                    margin-right: 15px;
+                    margin-top: 0;
+                    left: 0;
+                    display: none;
+                }
+                
+                .dropdown-submenu .dropdown-item {
+                    padding-left: 20px;
+                }
+            }
+        </style>`
     }
 }
 
 class Links extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=domain" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=domain">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="./reuse_elements/common.css">
-        <link rel="stylesheet" href="./reuse_elements/hover.css">
         `
     }
 }
@@ -90,69 +138,100 @@ class SpecialFooter extends HTMLElement {
                     <div><li class="desc_item"><i class="fa-solid fa-building-columns"></i><a href="https://www.vvitguntur.com/">VVIT</a></li></div>
                     <div><li class="desc_item"><i class="fa-solid fa-location-dot"></i><a href="https://www.google.com/maps/place/Vasireddy+Venkatadri+Institute+of+Technology/data=!4m2!3m1!1s0x0:0x8c86e4f36490336b?sa=X&ved=1t:2428&ictx=111">Guntur</a></li></div>
                     <div><li class="desc_item"><i class="fas fa-envelope"></i><a href="mailto:">Mail</a></li></div>
-                    <div><li class="desc_item"><i class="fa-brands fa-github"></i></i><a href="https://github.com/AIHUB-VVIT">Github</a></li></div>
+                    <div><li class="desc_item"><i class="fa-brands fa-github"></i><a href="https://github.com/AIHUB-VVIT">Github</a></li></div>
                 </ul>
             </div>
-        </nav>`
+        </nav>
+        <style>
+            .desc_item {
+                margin: 0 15px;
+                display: flex;
+                align-items: center;
+            }
+            
+            .desc_item i {
+                margin-right: 8px;
+            }
+            
+            .desc_item a {
+                text-decoration: none;
+                color: inherit;
+            }
+            
+            .desc_item a:hover {
+                color:rgb(214, 219, 228);
+            }
+            
+            #footer {
+                justify-content: center;
+                padding: 10px 0;
+            }
+            
+            @media (max-width: 768px) {
+                .desc_item {
+                    margin: 0 10px;
+                    font-size: 0.9em;
+                }
+            }
+        </style>`
     }
 }
-
-// Stylesheet for common elements
-const commonStyles = `
-<style>
-    .desc_item {
-        margin: 0 15px;
-        display: flex;
-        align-items: center;
-    }
-    
-    .desc_item i {
-        margin-right: 8px;
-    }
-    
-    .desc_item a {
-        text-decoration: none;
-        color: inherit;
-    }
-    
-    .desc_item a:hover {
-        color:rgb(214, 219, 228);
-    }
-    
-    #footer {
-        justify-content: center;
-        padding: 10px 0;
-    }
-    
-    @media (max-width: 768px) {
-        .desc_item {
-            margin: 0 10px;
-            font-size: 0.9em;
-        }
-    }
-    
-    /* Style for the dropdown headers */
-    .dropdown-header {
-        font-weight: bold;
-        color: #0d6efd;
-    }
-    
-    /* Add some padding to the indented items */
-    .ps-4 {
-        padding-left: 1.5rem !important;
-    }
-</style>
-`;
-
-// Add the common styles to the document
-document.head.insertAdjacentHTML('beforeend', commonStyles);
 
 // Define custom elements
 customElements.define('special-header', SpecialHeader);
 customElements.define('required-links', Links);
 customElements.define('special-footer', SpecialFooter);
 
-// Initialize Bootstrap dropdowns
+// Initialize Bootstrap dropdowns and handle nested dropdowns
 document.addEventListener('DOMContentLoaded', function() {
-    // Bootstrap's dropdowns will be automatically initialized by bootstrap.bundle.js
+    // Initialize all dropdowns for main nav items
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.nav-link.dropdown-toggle'));
+    dropdownElementList.map(function(dropdownToggleEl) {
+        return new bootstrap.Dropdown(dropdownToggleEl);
+    });
+    
+    // Handle dropdown submenus for both desktop and mobile
+    var dropdownSubmenus = document.querySelectorAll('.dropdown-submenu');
+    dropdownSubmenus.forEach(function(submenu) {
+        var dropdownToggle = submenu.querySelector('.dropdown-toggle');
+        var dropdownMenu = submenu.querySelector('.submenu');
+        
+        // Desktop hover effect
+        submenu.addEventListener('mouseenter', function() {
+            if (window.innerWidth >= 992) {
+                dropdownMenu.style.display = 'block';
+            }
+        });
+        
+        submenu.addEventListener('mouseleave', function() {
+            if (window.innerWidth >= 992) {
+                dropdownMenu.style.display = 'none';
+            }
+        });
+        
+        // Mobile touch handling
+        dropdownToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            // Close all other open submenus first
+            document.querySelectorAll('.dropdown-submenu .submenu').forEach(function(menu) {
+                if (menu !== dropdownMenu) {
+                    menu.style.display = 'none';
+                }
+            });
+            
+            // Toggle submenu visibility
+            dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+    
+    // Close any open dropdown when clicking outside
+    document.addEventListener('click', function(e) {
+        if (!e.target.closest('.dropdown-submenu')) {
+            document.querySelectorAll('.dropdown-submenu .submenu').forEach(function(menu) {
+                menu.style.display = 'none';
+            });
+        }
+    });
 });
